e5665c25337257cf520b6c0c12dcdd086f9496ef
asuperidea <riptideytvids@gmail.com>
2026-08-30 12:05:30 -0400

    lets any url access (temp)

--- backend/main.py
+++ backend/main.py
@@ -25,11 +25,12 @@ origins = [
     "http://localhost:8001",   
     "http://localhost:5500",
     "https://assword.simoncrystal.dev"
+    
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
