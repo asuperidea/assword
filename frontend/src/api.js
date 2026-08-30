020bf67d5121f21a4c6987d690ba5e700ad371a8
RiptideVideos <riptideytvids@gmail.com>
2026-08-30 09:36:07 -0400

    api changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,36 +1,27 @@
 const baseURL = "https://assword-backend.simoncrystal.dev/"
 
-export async function APIloginStart(email) { 
-    const url = baseURL + "login/start?userEmail=" + email
-    
-    try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
+export async function APIloginStart(email) {
+  const url = baseURL + "login/start?userEmail=" + encodeURIComponent(email);
 
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
-    console.error(error.message);
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
   }
+  return response.json();
 }
+
 export async function APIloginValidate(email, authKey) {  }  // GET /login/validate
 export async function APIsignup(email, salt, authKey) {
-    const templete = new Request(baseURL + "signup", {
+    const response = await fetch(baseURL + "signup", {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ 
-            email: email,
-            salt: salt,
-            authKey: authKey
-        }),
+        body: JSON.stringify({ email, salt, authKey }),
     });
 
-    const response1 = await fetch(templete);
-    console.log(response1.status);
-    
-    return response1;
-    }
+    if (! response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }    
+    return response.json();
+  }
 export async function APIcreateEntry(jwt, title, ciphertext, iv) { }  // POST /entry/new
 export async function APIgetEntries(jwt) { } 
