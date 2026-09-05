fe8313bb118f6c935d1c9cbff42bb5621e7cf4f7
asuperidea <riptideytvids@gmail.com>
2026-09-05 17:14:15 -0400

    working get entries (need decrypt)

--- frontend/src/valut.js
+++ frontend/src/valut.js
@@ -1,21 +1,50 @@
 import { APIgetEntries, APIcreateEntry } from './api.js';
 import { deriveAuthKey } from './crypto.js';
 
-async function getEntries(){
-    const jwt = sessionStorage.getItem("jwt");
-    const response = await APIgetEntries(jwt);
-    return response;
-}
-
 async function createEntry(title, password, masterPassword){
     const jwt = sessionStorage.getItem("jwt");
     const salt = sessionStorage.getItem("salt");
     try {
-        const response = APIcreateEntry(jwt, salt, title, password, masterPassword);
-        
+        const response = await APIcreateEntry(jwt, salt, title, password, masterPassword);
+        console.log("entry logged!");
+        return response;
     }catch (error) {
-        
+        console.log("CATCHING ERROR FROM VALUT.JS");
     }
 }
 
-getEntries();
+async function getEntries(){
+    const jwt = sessionStorage.getItem("jwt");
+
+    try {
+        const response = await APIgetEntries(jwt);
+        return response;
+    }catch (error) {
+        console.log("CATCHING ERROR FROM VALUT.JS");
+    }
+}
+
+function displayEntries(entries){
+    const area = document.getElementById("passwordArea");
+    for (let i=0; i< entries.length; i++){
+        const childDiv = document.createElement("div");
+        childDiv.insertAdjacentHTML('beforeend', `
+            <h2>${entries[i].title}</h2>
+            <p>${entries[i].content}</p>`);
+        childDiv.id=`passwordDiv${i}`;
+        area.appendChild(childDiv);
+    }
+}
+
+async function runEntries(){
+    const entries = await getEntries();
+    displayEntries(entries);
+}
+async function runNewEntries(){
+    const title = document.getElementById("entryTitle").value;
+    const content = document.getElementById("entryContent");
+}
+runEntries();
+
+const button = document.getElementById("newEntryButton");
+button.addEventListener("click", runSignUp);
